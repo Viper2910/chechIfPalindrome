3ee7df9154e600e0d387fc40e8428671c8741680
Andrzej Rzeszut <andrzejrz556@gmail.com>
2020-01-19 08:59:22 +0100

    Create getInputFromUser method and implement its body.

--- src/CheckIfPalindrome.java
+++ src/CheckIfPalindrome.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 public class CheckIfPalindrome{
 
-    Scanner scanner = new Scanner(System.in);
+    // Take input from user
+    public String getInputFromUser(){
+
+        String input;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the string to check if it is a palindrome: \n");
+        input = scanner.nextLine();
+
+        return input;
+    }
+
+    
 
 }
